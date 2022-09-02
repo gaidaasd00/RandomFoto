529a748e41c3b72b8173f4cb6f2eab58f4aae945
gaidaasd00 <swiftgaida@list.ru>
2022-09-02 19:31:55 +0300

    1.all break

--- RandomFoto/ViewControllers /ViewController.swift
+++ RandomFoto/ViewControllers /ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = .white
-        return imageView
-    }()
+    @IBOutlet var imageView: UIImageView!
     
-    private let button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .white
-        button.setTitle("Random Photo", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        return button
-    }()
+    @IBOutlet var buttonNext: UIButton!
+    //    private let imageView: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.backgroundColor = .white
+//        return imageView
+//    }()
+//
+//    private let button: UIButton = {
+//        let button = UIButton()
+//        button.backgroundColor = .white
+//        button.setTitle("Random Photo", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        return button
+//    }()
     
     let colors: [UIColor] = [
         .systemPink,
@@ -36,15 +38,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemPink
-        view.addSubview(imageView)
-        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-        imageView.center = view.center
         
         getPhoto()
         
-        button.addTarget(
+        buttonNext.addTarget(
             self,
             action: #selector(buttonTapped),
             for: .touchUpInside
@@ -52,25 +50,31 @@ class ViewController: UIViewController {
         
     }
     
-    @objc func buttonTapped() {
+    
+    @IBAction func buttonTapped(){
         getPhoto()
-        
+
         view.backgroundColor = colors.randomElement()
     }
+//        @objc func buttonTapped() {
+//        getPhoto()
+//
+//        view.backgroundColor = colors.randomElement()
+//    }
     
-    override func viewDidLayoutSubviews() {
-        view.addSubview(button)
-        button.frame = CGRect(
-            x: 30,
-            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
-            width: view.frame.width-60,
-            height: 55
-        )
-    }
+//    override func viewDidLayoutSubviews() {
+//        view.addSubview(buttonNext)
+//        buttonNext.frame = CGRect(
+//            x: 30,
+//            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+//            width: view.frame.width-60,
+//            height: 55
+//        )
+//    }
     
     func getPhoto() {
         let urlString = "https://source.unsplash.com/random/600x600"
-        let url = URL(string: urlString)!
+        guard let url = URL(string: urlString) else { return }
         guard let data = try? Data(contentsOf: url) else { return }
         
         imageView.image = UIImage(data: data)
